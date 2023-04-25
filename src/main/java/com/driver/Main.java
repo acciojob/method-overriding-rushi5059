69d92b3ac69f935cc847afc8096b46fc604f0799
Rushikesh Pote <rushi5059@gmail.com>
2023-04-25 13:43:47 +0530

    Assignment Completed2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -14,7 +14,7 @@ public class Main {
     public static class B extends A{
         @Override
         public String meth(){
-            return "Method is overridden in Extended class B";
+            return "Method is overridden in Extendend class B";
         }
     }
 }
